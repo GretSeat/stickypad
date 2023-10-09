c8565fdfb9df76732e76b94a3faeaaf6e4e367c0
Garrett Stewart <garrettbstewart@gmail.com>
2023-10-09 10:45:04 -0400

    removed useless stuff

--- config/database.js
+++ config/database.js
@@ -3,12 +3,7 @@ require("dotenv").config({ path: "./config/.env" });
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.DB_STRING, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useFindAndModify: false,
-      useCreateIndex: true,
-    });
+    const conn = await mongoose.connect(process.env.DB_STRING);
 
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (err) {
